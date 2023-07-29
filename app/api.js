ea524fe899c81a99dc627cf2644c76ea57133285
J-Friedemann <friedemann.jonatha@gmail.com>
2023-07-29 14:02:27 -0300

    remoção de consoles

--- app/api.js
+++ app/api.js
@@ -1,11 +1,6 @@
-//const apiKey = process.env.API_KEY;
-//console.log(apiKey);
-
 export const WeatherData = async (city) => {
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-  console.log(apiKey);
-  console.log(apiWeatherURL);
 
   const response = await fetch(apiWeatherURL);
   const data = await response.json();
